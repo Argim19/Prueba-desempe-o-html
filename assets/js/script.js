8f2679d3fe818c78e4486f4a55013ab2cc3b9d27
josue blanco <telegramplus55@gmail.com>
2026-05-04 20:27:48 -0500

    update portafolio library

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.AOS) {
+    AOS.init({
+      duration: 600,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+      offset: 120,
+    });
+  } else {
+    console.error(
+      "AOS no está cargado. Revisa la ruta de assets/vendor/aos/aos.js",
+    );
+  }
+});
+
 const menuToggle = document.querySelector(".open-menu");
 const menuToggles = document.querySelector(".close-menu");
 const navMenu = document.querySelector(".nav");
